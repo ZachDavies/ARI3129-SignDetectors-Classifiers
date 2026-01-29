3f83fc790d2f4aa633f5dade24224a314510854b
ZachDavies <zdmalta@proton.me>
2026-01-30 00:54:10 +0100

    yolov8 and v10 implementations and yolov8 for sign condition

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Jupyter Lab Environment";
+  description = "YOLOv8 Pip-Based Environment (Binary Wheels)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -8,22 +8,45 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
-          (python3.withPackages (ps: with ps; [
-            ipykernel
-            jupyter
-            numpy
-            pandas
-            matplotlib
+          stdenv.cc.cc.lib
+          zlib
+          glib
+          libGL
+          libGLU
+          libxml2
+          libxslt
+          
+          xorg.libxcb
+          xorg.libX11
+          
+          linuxPackages.nvidia_x11
+          
+          (python3.withPackages (ps: [
+            ps.pip
+            ps.venvShellHook
+            ps.ipykernel
           ]))
         ];
 
+        venvDir = "./.venv";
+
         shellHook = ''
+          export LD_LIBRARY_PATH=/run/opengl-driver/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.glib.out}/lib:${pkgs.libGL}/lib:${pkgs.libGLU}/lib:${pkgs.xorg.libxcb}/lib:${pkgs.xorg.libX11}/lib:$LD_LIBRARY_PATH
           export PYTHONPATH=$PYTHONPATH:$(pwd)
+          
+          if [ -d .venv ]; then
+            source .venv/bin/activate
+          fi
+          
+          echo "Environment Ready."
         '';
       };
     };

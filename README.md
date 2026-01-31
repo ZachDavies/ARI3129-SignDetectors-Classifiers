7491cd1644df5bd0dbf3677ec7fa8d6ef52d4936
ZachDavies <zdmalta@proton.me>
2026-01-31 14:31:10 +0100

    README.md and flake

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # ARI3129-SignDetectors-Classifiers
+
+This repository contains the dataset preparation, training, and evaluation scripts for the ARI3129 Group Project: **Automatic Detection & Attribute Classification of Maltese Traffic Signs**.
+
+1.  **Object Detection (Task 2a):** Detecting traffic signs using Faster R-CNN, RF-DETR, SSD/SSDLITE, RetinaNET and YOLO version 8/10/11/12.
+2.  **Attribute Classification (Task 2b):** Detecting specific sign attributes, RF-DETR for "Sign Shape" (Circular, Traingular, Square etc.), Yolov12 for "Viewing Angle" (Front, Side and Back), Yolov11 for "Mounting Type" (Pole-Mounted/Wall-Mounted), Yolov8 for "Sign Condition" (Good, Weathered, Heavily Damaged).
+
+---
+
+## 📂 Repository Structure
+
+- **`signs_coco/`**: Contains the source images and COCO-format annotations (`result.json`).
+- **`signs.json`**: Label Studio export containing specific attribute data (required for Task 2b).
+- **`2a_yolov8_zach.ipynb`**: Training pipeline for the standard YOLOv8 object detector.
+- **`2a_yolov10_zach.ipynb`**: Comparative training pipeline using the newer YOLOv10 architecture.
+- **`2b_signcondition_zach.ipynb`**: Yolov8 pipeline for detecting sign conditions using custom attribute parsing.
+
+---
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "YOLOv8 Pip-Based Environment (Binary Wheels)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          stdenv.cc.cc.lib
+          zlib
+          glib
+          libGL
+          libGLU
+          libxml2
+          libxslt
+          
+          xorg.libxcb
+          xorg.libX11
+          
+          linuxPackages.nvidia_x11
+          
+          (python3.withPackages (ps: [
+            ps.pip
+            ps.venvShellHook
+            ps.ipykernel
+          ]))
+        ];
+
+        venvDir = "./.venv";
+
+        shellHook = ''
+          export LD_LIBRARY_PATH=/run/opengl-driver/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.glib.out}/lib:${pkgs.libGL}/lib:${pkgs.libGLU}/lib:${pkgs.xorg.libxcb}/lib:${pkgs.xorg.libX11}/lib:$LD_LIBRARY_PATH
+          export PYTHONPATH=$PYTHONPATH:$(pwd)
+          
+          if [ -d .venv ]; then
+            source .venv/bin/activate
+          fi
+          
+          echo "Environment Ready."
+        '';
+      };
+    };
+}
